63c32424638fcaadc4b1d8e95669bffbe2a74957
Krishna Murthy S <80413499+skm1312@users.noreply.github.com>
2021-10-09 14:18:50 -0600

    Final Commit with Comments Handled all the test cases and commands dynamically with changes to the JSON file.

--- src/index.js
+++ src/index.js
@@ -1,107 +1,84 @@
+//Importing express,body parser and fs
 var myExpress = require("express");
 var myParser = require("body-parser");
 var app = myExpress();
 const fs = require("fs");
-const { response } = require("express");
-const { parse } = require("path");
-const { Console } = require("console");
-//create global costants with the error codes
-
+//Error codes to handle the response cases
+const ERROR_CODE = 404;
+const SUCCESS_CODE = 200;
+//Reading data using fs from the json file and parsing it
 let rawdata = fs.readFileSync("sneakers.json", "utf-8");
 let data = JSON.parse(rawdata);
-
+//Binding application middleware with instance
 app.use(myParser.urlencoded({ extended: true }));
+//Different endpoints to handle the GET requests
+
+//This would handle something like: nike/air_max_1
 app.get("/:make/:model", function (request, response) {
   let sizeRet = makeModelSize(request.params);
   response.send(sizeRet);
 });
-
+//This would handle 2 instances like: nike/air_max_1/11 or jordan/jordan_11/cool_gray
 app.get("/:make/:model/:size", function (request, response) {
   let sizeAvail = makeModelSize(request.params);
   response.send(sizeAvail);
 });
-
+//This would handle something like: jordan/jordan_11/cool_gray/11
 app.get("/:make/:model/:color/:size", function (request, response) {
   let sizeAvailCol = makeModelColorwaySize(request.params);
   response.send(sizeAvailCol);
 });
 
-app.get("/:make", function (request, response) {
-  let params = new URLSearchParams(request.params);
-  let makeval = params.get("make");
-  console.log(makeval);
-  for (const key in data) {
-    console.log("Test");
-    if (key === makeval) {
-      console.log("inside if");
-      let modelObject = data[makeval];
-      //console.log(Object.keys(modelObject));
-      response.send(Object.keys(modelObject));
-      break;
-    } else {
-      response.send("404: Product Not found");
-    }
-  }
-});
-
+//This is the function that handles the endpoint where we have 2 or 3 params
 function makeModelSize(values) {
   let params = new URLSearchParams(values);
   let makeval = params.get("make");
   let modelval = params.get("model");
-  let sizeval;
+  let sizeval = params.get("size");
   let count = 0;
-  try {
-    sizeval = params.get("size");
-  } catch (err) {
-    console.log("No size val in params");
-  }
   for (const key in data) {
     if (key === makeval) {
       let modelObject = data[makeval];
       if (modelObject.hasOwnProperty(modelval)) {
-        //console.log(modelObject[modelval]);
         let sizeObject = modelObject[modelval];
-        //console.log(sizeObject);
         let typeSize = Array.isArray(sizeObject);
+        //The case where we have a nested non-array response (i.e., colors for shoes)
         if (!typeSize && sizeval == null) {
-          console.log("value" + Object.keys(sizeObject));
-          return "200:" + Object.keys(sizeObject);
-        } else {
-          //console.log("test");
-          let c = parseInt(sizeval, 10);
-          if (isNaN(c)) {
-            //console.log("ifTest");
+          return SUCCESS_CODE + ":" + Object.keys(sizeObject);
+        }
+        // This is the case where it can be an array and not have colors
+        else if (sizeval === null) {
+          return SUCCESS_CODE + ":" + sizeObject;
+        }
+        // This handles the case where we have the third parameter as color but not size.
+        else {
+          let castedNum = parseInt(sizeval, 10);
+          if (isNaN(castedNum)) {
             let colorObject = sizeObject[sizeval];
-            console.log(colorObject);
-            if (colorObject !== undefined) return "200: " + colorObject;
-            else {
-              return "404: Product Not available";
+            if (colorObject !== undefined) {
+              return SUCCESS_CODE + ":" + colorObject;
+            } else {
+              return ERROR_CODE + ": Product Not available";
             }
           }
         }
-        //console.log(Array.isArray(sizeObject));
-        if (sizeval === null) {
-          return "200: " + sizeObject;
-        }
+        //Checking for size avaibility if the loop continues post colors
         for (var i = 0; i < sizeObject.length; i++) {
           if (sizeObject[i] === sizeval) {
-            return "200: Available in size " + sizeval;
+            return SUCCESS_CODE + ": Available in size " + sizeval;
           } else {
             count++;
           }
         }
+        //Unable to find the size after parsing through the entire object
         if (count === sizeObject.length) {
-          return "404: Not Available in size " + sizeval;
+          return ERROR_CODE + ": Not Available in size " + sizeval;
         }
-      } else {
-        return "404: Product Not available";
       }
-    } else {
-      return "404: Product Not available";
     }
   }
 }
-
+//This is the function that handles the endpoint where we have 4 params
 function makeModelColorwaySize(values) {
   let params = new URLSearchParams(values);
   let makeval = params.get("make");
@@ -111,24 +88,23 @@ function makeModelColorwaySize(values) {
   let count = 0;
   for (const key in data) {
     if (key === makeval) {
-      //console.log(Obj[key])
       let modelObject = data[makeval];
       if (modelObject.hasOwnProperty(modelval)) {
-        //console.log(modelObject[param2])
         let colorWayObject = modelObject[modelval];
-        //console.log(colorWayObject)
+        //Checking if we have the specified color in the model
         if (colorWayObject.hasOwnProperty(colorval)) {
           let sizeObject = colorWayObject[colorval];
-          console.log(sizeObject);
+          //Checking if we have the required size
           for (var i = 0; i < sizeObject.length; i++) {
             if (sizeObject[i] === sizeval) {
-              return "200: Available in size " + sizeval;
+              return SUCCESS_CODE + ": Available in size " + sizeval;
             } else {
               count++;
             }
           }
+          //Unable to find the size after parsing through the entire object
           if (count === sizeObject.length) {
-            return "404: Not Available in size " + sizeval;
+            return ERROR_CODE + ": Not Available in size " + sizeval;
           }
         }
       }
@@ -137,5 +113,5 @@ function makeModelColorwaySize(values) {
 }
 
 //Start the server and make it listen for connections on port 8080
-
 app.listen(8080);
+console.log("Listening....");
